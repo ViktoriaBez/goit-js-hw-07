338585adc16c3a9ca72980f54c5b315625647934
ViktoriaBez <154977573+ViktoriaBez@users.noreply.github.com>
2024-05-26 16:45:40 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,7 @@ console.log(`Number of categories: ${allCategories.length}`);
 
 allCategories.forEach(item => {
   const itemName = item.querySelector('h2').textContent;
-  const countElements = item.querySelectorAll('ul > li')
+  const countElements = item.querySelectorAll('ul > li');
 
   console.log('Category:', itemName);
   console.log('Elements:', countElements.length);
