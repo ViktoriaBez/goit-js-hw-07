d74c5bab90fd9dfa61c39c9d762628158d3eb3c5
ViktoriaBez <154977573+ViktoriaBez@users.noreply.github.com>
2024-06-02 19:22:53 +0300

    task 6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,42 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+function createBoxes(amount) {
+  boxesContainer.innerHTML = '';
+
+  const arr = [];
+  let size = 30;
+  
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    size += 10;
+    arr.push(box);
+  }
+  boxesContainer.append(...arr);
+}
+
+createButton.addEventListener('click', () => {
+  const amount = parseInt(input.value, 10);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  } else {
+    alert('Please enter a number between 1 and 100.');
+  }
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+  input.value = '';
+}
