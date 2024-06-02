9523eecb43224fe913edcb2f557efc88093e0f15
ViktoriaBez <154977573+ViktoriaBez@users.noreply.github.com>
2024-06-02 18:30:09 +0300

    task 345

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', event => {
+
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    
+    if (email === '' || password === '') {
+        alert('All form fields must be filled in');
+        return;
+        }
+
+   event.preventDefault();
+   console.log(`Email: ${email}, Password: ${password} `);
+   form.reset();  
+});
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const color = document.querySelector('.color');
+const changeColor = document.querySelector('.change-color');
+
+changeColor.addEventListener('click', event => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = body.style.backgroundColor;
+});
